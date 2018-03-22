a777563ceb74ff71ff5630d06245e5e61998f31f
asher978 <ashershaheen@gmail.com>
2018-03-22 14:50:28 -0400

    clean up

--- app.js
+++ app.js
@@ -61,17 +61,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// adding events routes
-const eventRoutes = require('./routes/event-routes');
-app.use('/events', eventRoutes);
-
-// routes for meetup and google API
-const meetupRoutes = require('./routes/meetup-routes');
-app.use('/meetup', meetupRoutes);
-
-// routes for auth
-// const authRoutes = require('./routes/auth-routes');
-// app.use('/auth', authRoutes);
+// User routes
 const userRoutes = require('./routes/user-routes');
 app.use('/user', userRoutes);
 
@@ -87,70 +77,3 @@ const io = module.exports.io = require('socket.io')(server);
 const SocketManager = require('./sockets/SocketManager');
 io.on('connection', SocketManager);
 // ------------- END SOCKETS ----------------------
-
-
-
-// socket io setup
-/* const io = require('socket.io')(server);
-const users = [];
-const messages = [];
-io.on('connection', (socket) => {
-  console.log('A new connection', socket.id);
-
-  socket.on('send message', (message) => {
-    messages.push(message);
-    io.emit('message', {
-      user: message.user,
-      text: message.text,
-    });
-  });
-
-  socket.on('user join', (user) => {
-    if (users.indexOf(user) === -1) {
-      users.push(user);
-      console.log(users);
-    } else console.log('already exist');  
-    io.emit('user join', users);
-  });
-
-  socket.on('coding', (code) => {
-    socket.broadcast.emit('code', code);
-  });
-
-  socket.on('test check', (result) => {
-    io.emit('checked', result);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User left', socket.id);
-  });
-}) */
-
-// ---------------  OLD LOGIC  --------------------
-// io.sockets.on('connection', (socket) => {
-//   connections.push(socket);
-//   console.log('Currently connected: ' + socket.id + ' %s users online', connections.length);
- 
-//   socket.on('disconnect', () => {
-//     // splicing the index of the disconnected socket
-//     connections.splice(connections.indexOf(socket), 1);
-//     console.log('Disconnected: %s users remaining', connections.length);
-//   });
-
-//   socket.on('join room', (data) => {
-//     socket.join(data.room);
-//   });
-
-//   socket.on('leave room', (data) => {
-//     socket.leave(data.room);
-//   });
-
-//   socket.on('coding', (data) => {
-//     socket.broadcast.to(data.room).emit('code', data);
-//   });
-
-//   socket.on('messaging', (data) => {
-//     io.in(data.room).emit('message', data.message);
-//     console.log('message: ' + data.message);
-//   });
-// });
